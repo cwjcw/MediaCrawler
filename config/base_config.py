744eab3e7fddef0d9242d2f812965225db0eff76
cwjcw <zhuce@cuixiaoyuan.cn>
2025-02-05 16:01:52 +0800

    111

--- config/base_config.py
+++ config/base_config.py
@@ -11,8 +11,8 @@
 
 # 基础配置
 PLATFORM = "xhs"
-KEYWORDS = "婚纱旅拍"  # 关键词搜索配置，以英文逗号分隔
-LOGIN_TYPE = "phone"  # qrcode or phone or cookie
+KEYWORDS = "伯爵旅拍"  # 关键词搜索配置，以英文逗号分隔
+LOGIN_TYPE = "qrcode"  # qrcode or phone or cookie
 COOKIES = ""
 # 具体值参见media_platform.xxx.field下的枚举值，暂时只支持小红书
 SORT_TYPE = "popularity_descending"
@@ -64,7 +64,7 @@ MAX_CONCURRENCY_NUM = 1
 ENABLE_GET_IMAGES = False
 
 # 是否开启爬评论模式, 默认开启爬评论
-ENABLE_GET_COMMENTS = False
+ENABLE_GET_COMMENTS = True
 
 # 爬取一级评论的数量控制(单视频/帖子)
 CRAWLER_MAX_COMMENTS_COUNT_SINGLENOTES = 10
